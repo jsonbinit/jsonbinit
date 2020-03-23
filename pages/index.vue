780690dddb6a6a0491cdeebec12f7dcdf12ff0b1
Andrea Stagi <stagi.andrea@gmail.com>
2020-03-23 14:20:06 +0100

    First import

---
+++ pages/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        JSON {🗑} IT!
+      </h1>
+      <h2 class="subtitle">
+        Service to store JSON on the fly
+      </h2>
+      <client-only placeholder="Loading...">
+        <prism-editor class="codecontent" :code="code" language="js" :line-numbers="true" />
+      </client-only>
+      <div class="links">
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >Save ➡ 🗑</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import 'prismjs'
+import 'prismjs/themes/prism-tomorrow.css'
+import 'vue-prism-editor/dist/VuePrismEditor.css'
+
+import PrismEditor from 'vue-prism-editor'
+
+export default {
+  components: {
+    PrismEditor
+  },
+  data: () => {
+    return {
+      code: '',
+      linenumbers: true
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.codecontent {
+  height: 300px;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 70px;
+  color: white;
+  letter-spacing: 1px;
+  text-align: center;
+
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: white;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+  text-align: center;
+}
+
+.links {
+  padding-top: 15px;
+  text-align: center;
+}
+</style>
