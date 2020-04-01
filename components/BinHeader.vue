f6237256b744e2edd5830edca8a8c7799cb65fd3
mino89 <pierdomenicoreitano@gmail.com>
2020-04-01 21:28:45 +0200

    added typography

--- components/BinHeader.vue
+++ components/BinHeader.vue
@@ -5,9 +5,6 @@
         JSON{🗑}IT!
       </h1>
     </nuxt-link>
-    <h2 class="subtitle">
-      <slot></slot>
-    </h2>
     <github-corner repo-url="https://github.com/jsonbinit" color="#1c1e20"/>
   </header>
 </template>
--- pages/docs/index.vue
+++ pages/docs/index.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="container">
     <div class="container__main">
-      <bin-header>
-        API Documentation
-      </bin-header>
+      <div class="header-area">
+        <bin-header />
+      </div>
+      <div class="jumbo">
+        <h1>API Documentation</h1>
+      </div>
+      <div class="content">
+        <h1>Title</h1>
+        <h2>Subtitle</h2>
+        <h3>Title 3</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo excepturi placeat doloribus, quidem voluptas, dolor cupiditate in a aperiam molestias minima. Dolorem iste deleniti cum laborum praesentium culpa quaerat enim.</p>
+        <ul>
+          <li>asdasd</li>
+          <li>asdasd</li>
+          <li>asdasd</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
