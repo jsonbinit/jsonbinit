8edfe8ec68c14b36029e0bfc0f2590aab0e84532
Andrea Stagi <stagi.andrea@gmail.com>
2020-04-09 19:25:47 +0200

    Rename Loading component

---
+++ components/Loading.vue
@@ -0,0 +1,23 @@
+<template>
+  <transition name="fade">
+    <div v-if="loading" class="loading-page">
+      <p><font-awesome-icon :icon="['fas', 'spinner']" pulse /></p>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    loading: false
+  }),
+  methods: {
+    start () {
+      this.loading = true
+    },
+    finish () {
+      this.loading = false
+    }
+  }
+}
+</script>
