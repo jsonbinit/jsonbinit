d79247b2c63449eeee0066215b38efd5b8e70f3b
Ibrahim Abdullahi Aliyu <Ibrahiimaa05@gmail.com>
2020-07-16 13:24:24 +0100

    Created ThemeSwitcher component

---
+++ components/ThemeSwitcher.vue
@@ -0,0 +1,35 @@
+<template>
+  <font-awesome-icon class="themer" :icon="['fa', 'sun']" @click="changeTheme" />
+</template>
+
+<script>
+export default {
+  methods: {
+    changeTheme () {
+      const html = document.getElementsByTagName('html')[0]
+      if (html.className === 'light-mode') {
+        html.className = 'not-dark'
+      } else {
+        html.className = 'light-mode'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.themer {
+  position: relative;
+  top: 0px;
+  cursor: pointer;
+  padding: 7px;
+  color: #ededed;
+  border: 2px solid;
+  margin: 0;
+  border-radius: 5px;
+  transition: all 0.1s ease;
+}
+.themer:hover {
+  top: -3px;
+}
+</style>
